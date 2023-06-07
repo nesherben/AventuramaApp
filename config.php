938ee3635a0d7a9b7834b031ce5265eaa4ab7505
Néstor Hernández Benito <nestor8299@gmail.com>
2023-06-08 00:14:16 +0200

    base

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ define("user", "root");
 define("pass", "");
 define("charset", "utf8mb4");
 */
-define("baseUrl", "http://localhost:8080/");
+//define("baseUrl", "http://localhost:8080/");
 
 define("host", "mysql-5707.dinaserver.com");
 define("db", "aventurama");
@@ -17,7 +17,7 @@ define("user", "nesherben");
 define("pass", "");
 define("charset", "utf8mb4");
 
-//define("baseUrl", "http://tfgnhb.endinahosting.com/");
+define("baseUrl", "http://tfgnhb.endinahosting.com/");
 define("emailHost", "tfgnhb@tfgnhb.endinahosting.com");
 
 ?>
